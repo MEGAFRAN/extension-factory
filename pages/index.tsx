5d65823659489525bdb495e3a7fb72e904384318
Francisco cruz <megafran@msn.com>
2023-05-11 14:49:19 -0500

    feature: version 0.0.0.4

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,39 +2,66 @@ import Head from 'next/head';
 import styles from '../app/styles/Home.module.scss';
 import { ButtonPanel } from '../app/components/button_panel/button_panel';
 
-const clubtalEnvironments = {
-  urls: ['https://www.clubtal.com','https://dev.clubtal.com'],
-  names: ['Produccion', 'Desarrollo']
-}
-const clubtalRepos = {
-  urls: ['https://github.com/MEGAFRAN/clubtal'],
-  names: ['Github Sitio web']
-}
-const clubtalJira = {
-  urls: [
-  'https://clubtal.atlassian.net/jira/software/projects/WA/boards/15',
-  'https://clubtal.atlassian.net/jira/software/c/projects/HR/boards/3',
-  'https://clubtal.atlassian.net/jira/software/projects/DES/boards/8',
-  'https://clubtal.atlassian.net/jira/software/projects/IDEA/boards/9',
-  'https://clubtal.atlassian.net/jira/software/projects/ON/boards/10'
-  ],
-  names: ['Web AI', 'Hr', 'Design', 'Idea', 'On Boarding']
-}
-const clubtalConfluence = {
-  urls: [
-  'https://clubtal.atlassian.net/wiki/spaces/WEB/overview',
-  'https://clubtal.atlassian.net/wiki/spaces/TEC/overview'
-  ],
-  names: ['General', 'Software']
-}
-const clubtalTools = {
-  urls: [
-  'https://www.deepl.com/translator',
-  'https://chat.openai.com/chat',
-  'https://www.google.com/',
-  'https://www.youtube.com/'
-  ],
-  names: ['Traductor idiomas', 'Chat Gpt', 'Google', 'Youtube']
+type QuickLink = {
+  urls: string[];
+  names: string[];
+  title: string;
+};
+
+type QuickLinksData = {
+  [key: string]: QuickLink;
+};
+
+const quickLinksData: QuickLinksData = {
+  clubtalEnvironments : {
+    urls: ['https://www.clubtal.com','https://dev.clubtal.com'],
+    names: ['Produccion', 'Desarrollo'],
+    title: 'Clubtal websites'
+  },
+  clubtalRepos: {
+    urls: ['https://github.com/MEGAFRAN/clubtal'],
+    names: ['Github Sitio web'],
+    title: 'Code repos'
+  },
+  clubtalProjects : {
+    urls: [
+    'https://clubtal.atlassian.net/jira/software/projects/WA/boards/15',
+    'https://clubtal.atlassian.net/jira/software/projects/MAR/boards/17',
+    'https://clubtal.atlassian.net/jira/software/c/projects/HR/boards/3',
+    'https://clubtal.atlassian.net/jira/software/projects/DES/boards/8',
+    'https://clubtal.atlassian.net/jira/software/projects/IDEA/boards/9',
+    'https://clubtal.atlassian.net/jira/software/projects/ON/boards/10'
+    ],
+    names: ['Web AI', 'Marketing', 'Hr', 'Design', 'Idea', 'On Boarding'],
+    title: 'Jira projects'
+  },
+  clubtalDocumentation : {
+    urls: [
+    'https://clubtal.atlassian.net/wiki/spaces/WEB/overview',
+    'https://clubtal.atlassian.net/wiki/spaces/TEC/overview'
+    ],
+    names: ['General', 'Software'],
+    title: 'Confluence documentation'
+  },
+  clubtalAnalytics : {
+    urls: [
+    'https://analytics.google.com/analytics/web/#/p326662031/reports/intelligenthome',
+    'https://tagmanager.google.com/#/container/accounts/6053376668/containers/91138574/workspaces/5',
+    'https://search.google.com/search-console?resource_id=sc-domain%3Aclubtal.com'
+    ],
+    names: ['Google analytics', 'Google tag manager', "Google search console"],
+    title: 'Analytics'
+  },
+  clubtalTools : {
+    urls: [
+    'https://www.deepl.com/translator',
+    'https://chat.openai.com/chat',
+    'https://www.google.com/',
+    'https://www.youtube.com/'
+    ],
+    names: ['Traductor idiomas', 'Chat Gpt', 'Google', 'Youtube'],
+    title: 'General tools'
+  }
 }
 
 export default function Home() {
@@ -47,16 +74,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <p>Sitios web clubtal</p>
-        <ButtonPanel numberOfButtons={clubtalEnvironments.names.length} arrayOfLinks={clubtalEnvironments.urls} arrayOfNames={clubtalEnvironments.names} />
-        <p>Repositorios</p>
-        <ButtonPanel numberOfButtons={clubtalRepos.names.length} arrayOfLinks={clubtalRepos.urls} arrayOfNames={clubtalRepos.names} />
-        <p>Projectos Jira</p>
-        <ButtonPanel numberOfButtons={clubtalJira.names.length} arrayOfLinks={clubtalJira.urls} arrayOfNames={clubtalJira.names} />
-        <p>Documentacion Confluence</p>
-        <ButtonPanel numberOfButtons={clubtalConfluence.names.length} arrayOfLinks={clubtalConfluence.urls} arrayOfNames={clubtalConfluence.names} />
-        <p>Herramientas</p>
-        <ButtonPanel numberOfButtons={clubtalTools.names.length} arrayOfLinks={clubtalTools.urls} arrayOfNames={clubtalTools.names} />
+        {
+          Object.keys(quickLinksData).map((key, index) => {
+            const {title, urls, names} = quickLinksData[key];
+            return (
+              <div key={index}>
+                <p>{title}</p>
+                <ButtonPanel numberOfButtons={names.length} arrayOfLinks={urls} arrayOfNames={names} />
+              </div>
+            );
+          })
+        }
       </main>
     </>
   )
